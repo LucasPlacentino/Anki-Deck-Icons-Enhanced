64008f3023aef10ab49e20f8ef9cf282a1b9deab
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2025-06-17 14:50:06 +0200

    Update __init__.py
    add AGPL notice

--- __init__.py
+++ __init__.py
@@ -1,3 +1,21 @@
+"""
+This program allows user-set icons to show in front of Anki decks in the desktop app, up to a user-defined sub-deck depth.
+Copyright (C) 2025  Lucas Placentino
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 # import the main window object (mw) from aqt
 from aqt import mw
 # import the "show info" tool from utils.py
